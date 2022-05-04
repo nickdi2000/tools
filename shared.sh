7473ff994ad32a8012718bdccfe13f980f2e37df
root <root@vps.server.local>
2022-05-04 14:49:59 +0000

    auto_update

--- shared.sh
+++ shared.sh
@@ -6,7 +6,17 @@ alias sss="source ~/.bashrc && echo sourced"
 function sel(){
 printf "Please select folder:\n"
 select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
-cd "$d" && pwd
+cd "$d/public_html" && ls && pwd
+}
+
+function savee(){
+	local curr = $(pwd)
+	cd /tools/
+	git add .
+	git commit -m "auto_update"
+	git push
+	source ~/.bashrc
+	cd "$curr"
 }
 
 alias savescript="git add . && git commit -m 'auto update' && git push && source ~/.bashrc"
